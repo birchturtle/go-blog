e0f777204c3e07521b7421f6b6e3872e85f56d9b
birchturtle <kj@birchturtle.net>
2021-05-16 11:27:44 +0200

    added / endpoint

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
+
+func index(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, Blog World (online, too!)")
+}
 
 func main() {
 	fmt.Println("Hello, Blog World!")
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
 }
